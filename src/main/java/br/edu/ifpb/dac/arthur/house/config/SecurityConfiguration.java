1fc0aeff7a504b9feddf05bd4ee25777efee9d75
Arthur Pereira da Silva <arthur.pereira@academico.ifpb.edu.br>
2023-04-19 11:33:54 -0300

    securityConfiguration is comming

--- src/main/java/br/edu/ifpb/dac/arthur/house/config/SecurityConfiguration.java
+++ src/main/java/br/edu/ifpb/dac/arthur/house/config/SecurityConfiguration.java
@@ -1,13 +1,80 @@
 package br.edu.ifpb.dac.arthur.house.config;
 
+import br.edu.ifpb.dac.arthur.house.business.interfaces.PasswordEncoderService;
+import br.edu.ifpb.dac.arthur.house.business.interfaces.SystemUserService;
 import br.edu.ifpb.dac.arthur.house.business.interfaces.TokenService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerA {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private TokenService tokenService;
+    private final TokenService tokenService;
+    private final SystemUserService systemUserService;
+    private final PasswordEncoderService passwordEncoderService;
+
+    public SecurityConfiguration(TokenService tokenService, SystemUserService systemUserService, PasswordEncoderService passwordEncoderService) {
+        this.tokenService = tokenService;
+        this.systemUserService = systemUserService;
+        this.passwordEncoderService = passwordEncoderService;
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    public TokenFilter jwtTokenFilter() {
+        return  new TokenFilter(this.tokenService, this.systemUserService);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(systemUserService).passwordEncoder(passwordEncoderService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf()
+        .disable()
+        .authorizeRequests()
+        .requestMatchers(CorsUtils::isPreFlightRequest)
+        .permitAll();
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterFilter() {
+        List<String> all = Arrays.asList("*");
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedMethods(all);
+        corsConfiguration.setAllowedOrigins(all);
+        corsConfiguration.setAllowedHeaders(all);
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        CorsFilter corsFilter = new CorsFilter(source);
+
+        FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<CorsFilter>(corsFilter);
+        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return  filter;
+
+    }
 }
